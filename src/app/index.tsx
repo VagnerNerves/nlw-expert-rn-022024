43c5b320079d24bfab87c7b8fc79eac4cb405755
Vagner Nerves <vagnernervessantos@gmail.com>
2024-02-06 07:42:53 -0300

    feat: Instaled prettier and config for ajusted code.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,9 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native'
 
 export default function Home() {
   return (
     <View>
       <Text>Hello React Native</Text>
     </View>
-  );
+  )
 }
