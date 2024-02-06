bf8dc9493b2706aaf433a6760101615d34045f5b
Vagner Nerves <vagnernervessantos@gmail.com>
2024-02-06 08:51:31 -0300

    feat: Add Category Button anda created flatlist with data.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,38 @@
-import { View } from 'react-native'
+import { FlatList, View } from 'react-native'
+
+import { useState } from 'react'
+
+import { CATEGORIES } from '@/utils/data/products'
 
 import { Header } from '@/components/header'
+import { CategoryButton } from '@/components/category-button'
 
 export default function Home() {
+  const [category, setCategory] = useState(CATEGORIES[0])
+
+  function handleCategorySelect(selectedCategory: string) {
+    setCategory(selectedCategory)
+  }
+
   return (
     <View className="flex-1 pt-8">
       <Header title="Faça seu pedido" cartQuantityItems={2} />
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelect(item)}
+          />
+        )}
+        horizontal
+        className="max-h-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
     </View>
   )
 }
