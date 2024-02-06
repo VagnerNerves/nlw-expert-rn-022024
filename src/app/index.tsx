95212646f0df1e9a26b9589907bceeb2d399f448
Vagner Nerves <vagnernervessantos@gmail.com>
2024-02-06 07:58:49 -0300

    feat: Configured layout.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from 'react-native'
 
 export default function Home() {
   return (
-    <View className="bg-slate-900 flex-1">
-      <Text className="text-white">Hello React Native</Text>
+    <View className="flex-1 pt-8">
+      <Text className="text-white text-xl">Hello React Native</Text>
     </View>
   )
 }
