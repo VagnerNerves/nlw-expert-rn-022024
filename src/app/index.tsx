07d8489fa48f52e3d60958c98a00abae205cd5d3
Vagner Nerves <vagnernervessantos@gmail.com>
2024-02-06 08:13:42 -0300

    feat: Configured font inter and loaded.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,9 @@ import { Text, View } from 'react-native'
 export default function Home() {
   return (
     <View className="flex-1 pt-8">
-      <Text className="text-white text-xl">Hello React Native</Text>
+      <Text className="text-white text-xl font-heading">
+        Hello React Native
+      </Text>
     </View>
   )
 }
