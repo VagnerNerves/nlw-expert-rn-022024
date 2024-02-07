bb3fc44888753ec27c7685822bbb94b591ec78c4
Vagner Nerves <vagnernervessantos@gmail.com>
2024-02-07 19:37:02 -0300

    feat: Ajusted for when empty product is not visible button send product.

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -28,6 +28,8 @@ export default function Cart() {
     )
   )
 
+  const isProducts = cartStore.products.length > 0
+
   function handleProductRemove(product: ProductCartProps) {
     Alert.alert('Remover', `Desejar remover ${product.title} do carrinho?`, [
       {
@@ -73,47 +75,53 @@ export default function Cart() {
       <KeyboardAwareScrollView>
         <ScrollView>
           <View className="p-5 flex-1">
-            {cartStore.products.length > 0 ? (
-              <View className="border-b border-slate-700">
-                {cartStore.products.map(product => (
-                  <Product
-                    key={product.id}
-                    data={product}
-                    onPress={() => handleProductRemove(product)}
-                  />
-                ))}
-              </View>
+            {isProducts ? (
+              <>
+                <View className="border-b border-slate-700">
+                  {cartStore.products.map(product => (
+                    <Product
+                      key={product.id}
+                      data={product}
+                      onPress={() => handleProductRemove(product)}
+                    />
+                  ))}
+                </View>
+
+                <View className="flex-row gap-2 items-center mt-5 mb-4">
+                  <Text className="text-white text-xl font-subtitle">
+                    Total:
+                  </Text>
+                  <Text className="text-lime-400 text-2xl font-heading">
+                    {total}
+                  </Text>
+                </View>
+
+                <Input
+                  placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento..."
+                  onChangeText={setAddress}
+                  blurOnSubmit={true}
+                  onSubmitEditing={handleOrder}
+                  returnKeyType="next"
+                />
+              </>
             ) : (
               <Text className="font-body text-slate-400 text-center my-8">
                 Seu carrinho está vazio.
               </Text>
             )}
-
-            <View className="flex-row gap-2 items-center mt-5 mb-4">
-              <Text className="text-white text-xl font-subtitle">Total:</Text>
-              <Text className="text-lime-400 text-2xl font-heading">
-                {total}
-              </Text>
-            </View>
-
-            <Input
-              placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento..."
-              onChangeText={setAddress}
-              blurOnSubmit={true}
-              onSubmitEditing={handleOrder}
-              returnKeyType="next"
-            />
           </View>
         </ScrollView>
       </KeyboardAwareScrollView>
 
       <View className="p-5 gap-5">
-        <Button onPress={handleOrder}>
-          <Button.Text>Enviar pedido</Button.Text>
-          <Button.Icon>
-            <Feather name="arrow-right-circle" size={20} />
-          </Button.Icon>
-        </Button>
+        {isProducts && (
+          <Button onPress={handleOrder}>
+            <Button.Text>Enviar pedido</Button.Text>
+            <Button.Icon>
+              <Feather name="arrow-right-circle" size={20} />
+            </Button.Icon>
+          </Button>
+        )}
 
         <LinkButton title="Voltar ao cardápio" href="/" />
       </View>
